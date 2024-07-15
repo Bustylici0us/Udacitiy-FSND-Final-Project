9c6616d1410d014f326f6650d646ae6d93c16146
Bustylici0us <161844907+Bustylici0us@users.noreply.github.com>
2024-07-15 13:34:02 +0200

    Update models.py

--- models.py
+++ models.py
@@ -6,11 +6,12 @@ database_path = os.getenv("DATABASE_NAME")
 db = SQLAlchemy()
 
 def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.app = app
-    db.init_app(app)
-    db.create_all()
+    with app.app_context():
+        app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        db.app = app
+        db.init_app(app)
+        db.create_all()
 
 def db_drop_and_create_all():
     db.drop_all()
