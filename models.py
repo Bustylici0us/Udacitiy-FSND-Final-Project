4be92e2055487a16515721e9e4f770bcef8243d8
Bustylici0us <161844907+Bustylici0us@users.noreply.github.com>
2024-07-15 12:43:00 +0200

    Update models.py

--- models.py
+++ models.py
@@ -5,64 +5,63 @@ import os
 database_path = os.getenv("DATABASE_NAME")
 db = SQLAlchemy()
 
-with app.app_context():
-    def setup_db(app, database_path=database_path):
-        app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-        db.app = app
-        db.init_app(app)
-        db.create_all()
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
 
-    def db_drop_and_create_all():
-        db.drop_all()
-        db.create_all()
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
     
-    class Movie(db.Model):
-        __tablename__ = 'movies'
-        id = db.Column(db.Integer, primary_key=True)
-        title = db.Column(db.String(120), nullable=False)
-        release_date = db.Column(db.Date, nullable=False)
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    release_date = db.Column(db.Date, nullable=False)
 
-        def insert(self):
-            db.session.add(self)
-            db.session.commit()
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
     
-        def update(self):
-            db.session.commit()
+    def update(self):
+        db.session.commit()
     
-        def delete(self):
-            db.session.delete(self)
-            db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
     
-        def format(self):
-            return {
-                'id': self.id,
-                'title': self.title,
-                'release_date': self.release_date
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
             }
     
-    class Actor(db.Model):
-        __tablename__ = 'actors'
-        id = db.Column(db.Integer, primary_key=True)
-        name = db.Column(db.String(120), nullable=False)
-        age = db.Column(db.Integer, nullable=False)
-        gender = db.Column(db.String(10), nullable=False)
+class Actor(db.Model):
+    __tablename__ = 'actors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
     
-        def insert(self):
-            db.session.add(self)
-            db.session.commit()
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
     
-        def update(self):
-            db.session.commit()
+    def update(self):
+        db.session.commit()
     
-        def delete(self):
-            db.session.delete(self)
-            db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
     
-        def format(self):
-            return {
-                'id': self.id,
-                'name': self.name,
-                'age': self.age,
-                'gender': self.gender
-            }
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
