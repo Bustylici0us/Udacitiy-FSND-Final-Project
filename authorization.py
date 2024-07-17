543ee6518b6c00c43134d9afcd709a4c549001fc
Bustylici0us <161844907+Bustylici0us@users.noreply.github.com>
2024-07-17 19:40:17 +0200

    Update authorization.py

--- authorization.py
+++ authorization.py
@@ -93,16 +93,18 @@ def verify_decode_jwt(token):
                      "description": "Unable to find the appropriate key."}, 400)
 
 def requires_auth(permission=''):
-    """Decorator method for requiring auth on endpoints"""
     def requires_auth_decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            print(f"Token: {token}")
             try:
-                token = get_token_auth_header()
                 payload = verify_decode_jwt(token)
+                print("Payload:", payload)
                 check_permissions(permission, payload)
             except AuthError as e:
-                abort(e.status_code, e.error['description'])
+                print("AuthError:", e.error)
+                return jsonify(e.error), e.status_code
             return f(payload, *args, **kwargs)
         return wrapper
     return requires_auth_decorator
